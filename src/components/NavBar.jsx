8a07b7020931706044ff60002027bd4bb2d47683
Sanaya James <sanaya.j.james@gmail.com>
2025-12-14 17:34:53 -0700

    Logout button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,13 @@
 import { NavLink } from "react-router-dom"
 import "./NavBar.css"
 
-function NavBar() {
+function NavBar({ logout }) {
 
   return (
     <nav>
       <NavLink
         to="/"
-        className="nav-link" 
+        className="nav-link"
       >
         Home
       </NavLink>
@@ -16,6 +16,10 @@ function NavBar() {
         className="nav-link"
       >
         About
+      </NavLink>
+      <NavLink>
+        {/* Add a button with the logout function to handle the onClick event */}
+        <button onClick={logout}>Logout</button>
       </NavLink>
     </nav>
   )
