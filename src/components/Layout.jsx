64a42a7e9abb3571aa4e399f207b2555b8559bce
Sanaya James <sanaya.j.james@gmail.com>
2025-12-14 17:32:13 -0700

    add user login/logout functionality. navigate to login

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,37 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import NavBar from "./NavBar"
+import { useState, useEffect } from "react"
 
 function Layout() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const navigate = useNavigate();
 
+  const login = () => {
+    setIsLoggedIn(true)
+  }
+
+  const logout = () => {
+    setIsLoggedIn(false)
+  }
+
+  //add programmatic naviagtion for login and logout
+  useEffect(() => {
+    if (isLoggedIn) {
+      //naviagtes to Home route if user is logged in
+      navigate("/")
+    } else {
+      //navigates to Login route if user is logged out
+      navigate("/login")
+    }
+  }, [isLoggedIn])
 
   return (
     <div className="layout">
-      <NavBar />
-      <Outlet />
+      {/* pass logout for the logout button */}
+      <NavBar logout={logout} />
+
+      {/* pass the login function to all routes via useOutletContext */}
+      <Outlet context={login} />
     </div>
   )
 }
